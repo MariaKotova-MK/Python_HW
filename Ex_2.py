bb97305501f79f37dcf1a838b160ff5d28b593d4
Mariia <MK193@yandex.ru>
2022-12-01 20:20:18 +0300

    HW1 added Ex_2

--- Ex_2.py
+++ Ex_2.py
@@ -1,2 +1,8 @@
 #Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
+for x in [True, False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            first = not(x or y or z)
+            second = (not(x) or not(y) or not(z))
+            print(f'{x:^7}{y:^7}{z:^7} --- {first:^7}|{second:^7} --- {(first==second):^7}')
