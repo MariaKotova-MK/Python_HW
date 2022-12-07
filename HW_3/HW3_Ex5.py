65697bbf62ed59a012f5f2ed9bddc40c0f360752
Mariia <MK193@yandex.ru>
2022-12-07 23:51:51 +0300

    HW 3 - ex_5 done

---
+++ HW_3/HW3_Ex5.py
@@ -0,0 +1,11 @@
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов (Негафибоначчи).
+# Пример: для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+size = int(input('Введите длину списка чисел Фибоначчи - '))
+first_fibo = [1, 0, 1]
+
+for i in range(2, size+2):
+    first_fibo.append((first_fibo[i-1]+first_fibo[i]))
+for i in range(size):
+    first_fibo.insert(0, first_fibo[1] - first_fibo[0])
+print(first_fibo)
